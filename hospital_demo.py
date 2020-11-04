4e0f2370f652bafb8ab5ce848852c8bbc406f0b5
Kwonkyu <park2348190@naver.com>
2020-11-04 18:10:44 +0900

    optimized code

--- hospital_demo.py
+++ hospital_demo.py
@@ -31,60 +31,54 @@ def demo():
     # check response code
     # print(response_tree[0][0].tag)
     # print(response_tree[0][0].attrib)
-    if response_tree[0][0].text != "00":
-        print("BAD RESPONSE FROM SERVER.")
+    result_code = response_tree.find("resultCode")
+    if result_code is None or result_code.text != "00":
+        print("BAD RESPONSE FROM SERVER - result code is not 00")
 
     # check body
-    body_tree = response_tree[1]
+    body_tree = response_tree.find("body")
+    if body_tree is None:
+        print("BAD RESPONSE FROM SERVER - body element not exist.")
 
     hospital_list = []
 
-    for body_element in body_tree:
-        if body_element.tag == "items":
-            # parse returned hospitals
-            for item in body_element:
-                hospital = Hospital()
-                for information in item:
-                    if information.tag == "distance":
-                        hospital.distance = information.text
-                    elif information.tag == "dutyAddr":
-                        hospital.address = information.text
-                    elif information.tag == "dutyDiv":
-                        hospital.level = information.text
-                    elif information.tag == "dutyDivName":
-                        hospital.facility = information.text
-                    elif information.tag == "dutyEmcls":
-                        hospital.emergency_code = information.text
-                    elif information.tag == "dutyFax":
-                        hospital.fax_number = information.text
-                    elif information.tag == "dutyLvkl":
-                        hospital.status = information.text
-                    elif information.tag == "dutyName":
-                        hospital.name = information.text
-                    elif information.tag == "dutyTel1":
-                        hospital.contact = information.text
-                    elif information.tag == "endTime":
-                        hospital.end_time = information.text
-                    elif information.tag == "hpid":
-                        hospital.hospital_id = information.text
-                    elif information.tag == "latitude":
-                        hospital.latitude = information.text
-                    elif information.tag == "longitude":
-                        hospital.longitude = information.text
-                    elif information.tag == "startTime":
-                        hospital.start_time = information.text
-                hospital_list.append(hospital)
-        elif body_element.tag == "numOfRows":
-            num_or_rows = body_tree[1].text
-        elif body_element.tag == "pageNo":
-            page_no = body_tree[2].text
-        elif body_element.tag == "totalCount":
-            hospital_count = body_tree[3].text
+    items_tree = body_tree.find("items")
+    if items_tree is not None:
+        for item in items_tree:
+            hospital = Hospital()
 
-    print("THERE ARE {} NUMBER OF ROWS PER PAGE.".format(num_or_rows))
-    print("CURRENT PAGE NUMBER IS {}.".format(page_no))
-    print("TOTAL ITEM COUNT IS {}.".format(hospital_count))
+            for information in item:
+                if information.tag == "distance":
+                    hospital.distance = information.text
+                elif information.tag == "dutyAddr":
+                    hospital.address = information.text
+                elif information.tag == "dutyDiv":
+                    hospital.level = information.text
+                elif information.tag == "dutyDivName":
+                    hospital.facility = information.text
+                elif information.tag == "dutyEmcls":
+                    hospital.emergency_code = information.text
+                elif information.tag == "dutyFax":
+                    hospital.fax_number = information.text
+                elif information.tag == "dutyLvkl":
+                    hospital.status = information.text
+                elif information.tag == "dutyName":
+                    hospital.name = information.text
+                elif information.tag == "dutyTel1":
+                    hospital.contact = information.text
+                elif information.tag == "endTime":
+                    hospital.end_time = information.text
+                elif information.tag == "hpid":
+                    hospital.hospital_id = information.text
+                elif information.tag == "latitude":
+                    hospital.latitude = information.text
+                elif information.tag == "longitude":
+                    hospital.longitude = information.text
+                elif information.tag == "startTime":
+                    hospital.start_time = information.text
+
+            hospital_list.append(hospital)
 
     print("END OF DEMO")
 
-    return render_template("hospital.html", item_count=hospital_count, items=hospital_list)
+    return render_template("hospital.html", item_count=len(hospital_list), items=hospital_list)
